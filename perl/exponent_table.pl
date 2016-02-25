f75bda5dd99885834d5410884fae412073d3a29c
Kent Fredric <kentfredric@gmail.com>
2016-02-25 16:24:29 +1300

    Replace range-max's with constants to make it more obvious their purpose

--- perl/exponent_table.pl
+++ perl/exponent_table.pl
@@ -3,13 +3,16 @@
 use strict;
 use warnings;
 
-use constant MAX_VAL => 1000;
+use constant MAX_VAL     => 1000;
+use constant BASE_MAX    => 30;
+use constant DIVISOR_MAX => 30;
+
 use bigint;
 
 # Computes a table of data for pretty printing later
 my %computed;
-for my $base ( 2 .. 30 ) {
-  for my $divisor ( 2 .. 30 ) {
+for my $base ( 2 .. BASE_MAX ) {
+  for my $divisor ( 2 .. DIVISOR_MAX ) {
     for my $multiple ( 1 .. MAX_VAL ) {
       my $exponent = $base**$multiple;
       last if $exponent > MAX_VAL;
