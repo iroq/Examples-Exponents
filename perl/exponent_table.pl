b10eef1fcd4c020ee4df90b66a90fdb8a4cbe9b5
Kent Fredric <kentfredric@gmail.com>
2016-02-25 16:18:20 +1300

    Tidy up code and logic slightly

--- perl/exponent_table.pl
+++ perl/exponent_table.pl
@@ -6,23 +6,25 @@ use warnings;
 use constant MAX_VAL => 1000;
 use bigint;
 
+# Computes a table of data for pretty printing later
 my %computed;
-
 for my $base ( 2 .. 30 ) {
   for my $divisor ( 2 .. 30 ) {
     my @matches;
     for my $multiple ( 1 .. MAX_VAL ) {
-      my $exponent = $base ** $multiple;
+      my $exponent = $base**$multiple;
       last if $exponent > MAX_VAL;
-      push @matches, $exponent if $exponent % $divisor == 0;
+      next if $exponent % $divisor;
+      push @{ $computed{$base}{$divisor} }, $exponent;
     }
-    $computed{$base}{$divisor} = \@matches if @matches;
   }
 }
 
+# Pretty Prints Accumulated data.
 for my $base ( sort { $a <=> $b } keys %computed ) {
   printf "exponents of %2d:\n", $base;
   for my $divisor ( sort { $a <=> $b } keys %{ $computed{$base} } ) {
-    printf "  divisible by %2d: %s\n", $divisor, join q[ ], map { sprintf "%4d,", $_ } @{ $computed{$base}{$divisor} };
+    printf "  divisible by %2d: %s\n", $divisor, join q[ ],
+      map { sprintf "%4d,", $_ } @{ $computed{$base}{$divisor} };
   }
 }
